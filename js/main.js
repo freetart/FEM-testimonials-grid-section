b0874387786cbea0eae58779d709968bcf371d11
habib <habibdev96@gmail.com>
2021-05-14 23:08:53 -0600

    created preloader

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// preloader
+const preloader = () => {
+  const preloaderEl = document.querySelector(".preloader");
+
+  window.addEventListener("load", function () {
+    preloaderEl.classList.add("hide-preloader");
+  });
+};
+preloader();
